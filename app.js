4804ccbfc3656dc463fa9ed83472e54cae1ea760
hervekwizera <hervekwizera63@gmail.com>
2025-03-26 12:58:15 +0200

    starting our express server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('saver running on localhost:3000');
+import express from 'express';
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('welcome to the subscription tracker API!');
+})
+
+app.listen(3000,()=>{
+    console.log('Subscription tracker API is running on http://localhost:3000');
+})
+ export default app;
