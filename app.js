b3d0c67e27fd1189eab097914640ba683f8fe17c
hervekwizera <hervekwizera63@gmail.com>
2025-03-26 14:34:41 +0200

    configuring environmental variable

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import { PORT } from './config/env.js';
+
 const app = express();
 
 app.get('/',(req,res)=>{
     res.send('welcome to the subscription tracker API!');
 })
 
-app.listen(3000,()=>{
-    console.log('Subscription tracker API is running on http://localhost:3000');
+app.listen(PORT,()=>{
+    console.log(`Subscription tracker API is running on http://localhost:${PORT}`);
 })
  export default app;
