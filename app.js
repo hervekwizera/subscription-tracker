2ee84a4ca6b6892009d8dc8c0559de158836d0d7
hervekwizera <hervekwizera63@gmail.com>
2025-04-08 10:26:26 +0200

    importing middlewares we need in project

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import { PORT } from './config/env.js';
 import userRouter from './routes/user.routes.js'
 import authRouter from './routes/auth.routes.js'
@@ -7,7 +8,9 @@ import connectToDataBase from './database/mongodb.js'
 
 
 const app = express();
-
+  app.use(express.json());
+  app.use(express.urlencoded({extended:false}));
+  app.use(cookieParser())
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
