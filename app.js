a338155ee5e099fd3217e16912028035e72e3b64
hervekwizera <hervekwizera63@gmail.com>
2025-04-12 14:58:17 +0200

    adding error middelware in app.js to be used

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
 
+app.use(errorMiddleware);
+
 app.get('/',(req,res)=>{
     res.send('welcome to the subscription tracker API!');
 })
