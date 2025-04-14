cae0ce622eafeaf5cd3bf699a4101c6883d44b77
hervekwizera <hervekwizera63@gmail.com>
2025-04-14 08:15:18 +0200

    fixing the bug in user middleware: timestamp => timestamps

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -6,8 +6,8 @@ import User from '../models/user.model.js';
 const authorize = async (req,res,next)=>{
        try {
         let token;
-        if(req.headers.authorization && req.headers.authorization.startWith('Bearer')){
-            token = req.headers.authorization.split('')[1];
+        if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+            token = req.headers.authorization.split(' ')[1];
         }
         if(!token) return res.status(401).json({message:'Unauthorized'});
 
--- models/user.model.js
+++ models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
         required: [true,'User Password is required'],
         minLength: 6,
     }
-},{ timestamp: true});
+},{ timestamps: true});
 
 const User = mongoose.model('User',userSchema);
 
