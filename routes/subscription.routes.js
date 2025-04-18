69eee56020fc4a603cc1e39313f94d16ab40895e
hervekwizera <hervekwizera63@gmail.com>
2025-04-18 13:53:13 +0200

    .

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import authorize from '../middlewares/auth.middleware.js'
 
 const subscriptionRouter = Router();
 
@@ -6,7 +7,7 @@ subscriptionRouter.get('/',(req,res)=> res.send({title:'GET all subscriptions'})
 
 subscriptionRouter.get('/:id',(req,res)=> res.send({title:'GET subscription details'}))
 
-subscriptionRouter.post('/',(req,res)=> res.send({title:'CREATE subscription'}))
+subscriptionRouter.post('/',authorize,(req,res)=> res.send({title:'CREATE subscription'}))
 
 subscriptionRouter.put('/:id',(req,res)=> res.send({title:'UPDATE subscription'}))
 
