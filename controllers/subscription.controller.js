dfe805b7fc020d2137c6c4446e5acc760bb7530e
hervekwizera <hervekwizera63@gmail.com>
2025-04-18 13:42:14 +0200

    add try and catch block

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,3 +1,10 @@
 export const  createSubscription = (req,res,next)=>{
-    
+    try {
+        const subscription = await Subscription.create({
+            ...req.body,
+            user: req.user.id,
+        })
+    } catch (e) {
+        next(e);
+    }
 }
