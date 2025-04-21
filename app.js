d39d33fd810ea314419986bc7ccadac3ad7af9a8
hervekwizera <hervekwizera63@gmail.com>
2025-04-21 08:05:44 +0200

    creation of workflow controller

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDataBase from './database/mongodb.js'
 import errorMiddleware from './middlewares/error.middleware.js';
 import arcjetMiddleware from './middlewares/arcjet.middleware.js';
+import workflowRouter from './routes/workflow.route.js';
 
 
 const app = express();
@@ -18,7 +19,7 @@ const app = express();
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
-
+app.use('/api/v1/workflows', workflowRouter);
 app.use(errorMiddleware);
 
 app.get('/',(req,res)=>{
