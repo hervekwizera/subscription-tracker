076b80b72d31c0a7095d4f2770eedc22f8cb681c
hervekwizera <hervekwizera63@gmail.com>
2025-04-25 05:47:40 +0200

    writting some code in workflow.controller.js

--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -1,8 +1,15 @@
 import {createRequire} from 'modele';
-
 const require = createRequire (import.meta.url);
+const {serve} = require('@upstash/workflow/express');
 
 
 export const sendReminders = serve(async (context)=>{
+    const {subscriptionId} = context.requestPayload;
+    const subscription = await fetchSubscription(context,subscriptionId);
+    if (!subscription || subscription.status !== active) return;
 
-});
+    const renewalDate =
+
+} );
+
+
