993583bc49cfbafbc24408386ce291a18af83af4
hervekwizera <hervekwizera63@gmail.com>
2025-05-01 10:41:29 +0200

    .

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,5 +1,9 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    
+    service: 'gmail',
+    auth: {
+        user:'hervekwizera63@gmail.com',
+        pass: EMAIL_PASSWORD
+    }
 })
