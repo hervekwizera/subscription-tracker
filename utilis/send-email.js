2e2a4432a9ce281a61215c805475605bb05d217d
hervekwizera <hervekwizera63@gmail.com>
2025-05-02 10:01:51 +0200

    writting code for mailInfor

--- utilis/send-email.js
+++ utilis/send-email.js
@@ -1,3 +1,15 @@
 export const sendReminderEmail = async({to,type,subscription})=>{
+ if(!to || !type) throw new Error('Missing required parameters');
+ const template = emailTemplates.find((t)=>t.label===type);
+ if(!template)throw new Error('Invalid email type');
+
+ const mailInfor ={
+    userName: subscription.user.name,
+    subscriptionName: subscription.name,
+    renewaldate: dayjs(subscription.renewalDate).format('MMM D,YYYY'),
+    PlanName: subscription.name,
+    price:`${subscription.currency} ${subscription.price} (${subscription.frequency})`, 
+    paymentMethod: subscription.paymentMethode,
+ }
  
 }
